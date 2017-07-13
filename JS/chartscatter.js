56a7e12f6694e045e0d6c05f542c5668b3e46361
mcamp <usmcamp0811@gmail.com>
2017-07-13 12:45:20 -0500

    loading data

--- JS/chartscatter.js
+++ JS/chartscatter.js
@@ -2,5 +2,26 @@
  * Created by mcamp on 7/13/17.
  */
 
-var data = d3.json("../Data/scatter_plot_example_data.json", function(d) { d; });
+// var circle_data = d3.json("../Data/scatter_plot_example_data.json", function(d) { d; });
 
+
+circle_data = [{"x": 100,
+    "y": 300,
+    "r": 10},
+    {"x": 200,
+        "y": 250,
+        "r": 10},
+    {"x": 300,
+        "y": 180,
+        "r": 10}]
+
+
+svg = d3.select("svg");
+diag_circles = svg.selectAll("circle");
+
+diag_circles.data(circle_data)
+    .enter()
+    .append("circle")
+    .attr("cx", function(d){return d.x})
+    .attr("cy", function(d){return d.y})
+    .attr("r", function(d){return d.r});
