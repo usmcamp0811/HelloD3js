67678680fcb8101ae019688da8298560fbec6d89
mcamp <usmcamp0811@gmail.com>
2017-07-14 15:40:53 -0500

    learning to code JS

--- JS/array_loops.js
+++ JS/array_loops.js
@@ -1,3 +1,12 @@
 /**
  * Created by mcamp on 7/14/17.
  */
+var myList = ['apples', 'oranges', 'bannanas'];
+
+myList[3] = 'pineapples';
+
+myList[0] = 'watermelon';
+
+myList.push('add more fruit');
+
+alert("myLists currently holds the following: " + myList);
