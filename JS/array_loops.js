0f7a3ea806fbc9534da429f3dfaa1395466bcc81
mcamp <usmcamp0811@gmail.com>
2017-07-14 16:29:09 -0500

    learning to code JS

--- JS/array_loops.js
+++ JS/array_loops.js
@@ -9,4 +9,25 @@ myList[0] = 'watermelon';
 
 myList.push('add more fruit');
 
-alert("myLists currently holds the following: " + myList);
+function go() {
+    alert("WHAT?!! You can add functions to a list?! Craziness!!")
+}
+
+myList[4] = go
+
+var anotherList = ["Matt", "Candace"];
+
+myList[5] = anotherList;
+
+function showList(){
+    alert("myLists currently holds the following: " + myList);
+};
+
+
+var ul = d3.select('body').append('ul');
+
+ul.selectAll('li')
+    .data(myList)
+    .enter()
+    .append('li')
+    .html(String);
