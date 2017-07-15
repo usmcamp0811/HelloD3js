950c17fd486e469cf4f836d53b9943f4c6610517
usmcamp0811 <usmcamp0811@gmail.com>
2017-07-14 22:09:56 -0500

    A good night of learning...

--- JS/array_loops.js
+++ JS/array_loops.js
@@ -1,33 +1,63 @@
 /**
  * Created by mcamp on 7/14/17.
  */
-var myList = ['apples', 'oranges', 'bannanas'];
+var myList = ['this', 'is', 'a', 'listArray'];
 
-myList[3] = 'pineapples';
-
-myList[0] = 'watermelon';
-
-myList.push('add more fruit');
+function clearlist(){
+    myList = [];
+    console.log("cleared");
+    console.log(myList);
+    showList();
+}
 
 function go() {
     alert("WHAT?!! You can add functions to a list?! Craziness!!")
 }
 
-myList[4] = go
+function replace() {
+    var index = prompt("Whats the index for the item you want to replace?");
+    var item = prompt("Input a value.");
+    myList[index] = item;
+    showList();
+}
 
-var anotherList = ["Matt", "Candace"];
+function add() {
+    var input = prompt("Input a value to add to the list.");
+    myList.push(input);
+    console.log("added item")
+    showList();
+}
 
-myList[5] = anotherList;
+function remove() {
+    var index = prompt("What's the index of the item to remove?");
+    delete myList[index];
+    showList();
+
+}
+
+function addfunc() {
+    myList.push(go);
+    showList();
+}
+
+function addlist() {
+    var anotherList = ['This', 'is', 'another', 'list'];
+    myList.push(anotherList);
+    showList();
+}
 
 function showList(){
-    alert("myLists currently holds the following: " + myList);
-};
+    var ul = d3.select('#list');
 
+    ul.selectAll('li').remove();
 
-var ul = d3.select('body').append('ul');
+    ul.selectAll('li')
+        .data(myList)
+        .enter()
+        .append('li')
+        .html(String);
+}
 
-ul.selectAll('li')
-    .data(myList)
-    .enter()
-    .append('li')
-    .html(String);
+showList();
+document.getElementById("clear_btn").onclick = clearlist;
+document.getElementById("add_btn").onclick = add;
